cbcf13836b913b389a5e387f08f12f23a266e518
Aron Ambrosiani <aron@ambrosiani.se>
2017-05-05 14:22:35 +0200

    initial batch of beacons

--- app/data/beaconBlockRules.js
+++ app/data/beaconBlockRules.js
@@ -1,238 +1,81 @@
 
 export const blockRules = [
   {
-    uuid: '20688:13234', // Testing Beacon
-    region: 'blue',
-    floor: 7,
-    blocks: [],
-  },
-  {
-    uuid: '54351:29236', // Beacon #1
-    region: 'blue',
-    floor: 7,
-    blocks: [],
-  },
-  {
-    uuid: '10198:12437', // Beacon #3
-    region: 'red',
-    floor: 7,
-    blocks: [
-      // Green
-      '8184:46711', // Beacon #9
-      '31392:9354', // Beacon #10
-      // Orange
-      '8696:20374', // Beacon #12
-      // Blue
-      '54351:29236', // Beacon #1
-      '38857:8717', // Beacon #13
-    ],
-  },
-  {
-    uuid: '34942:3069', // Beacon #4
-    region: 'pink',
-    floor: 7,
-    blocks: [
-      // Green
-      '8184:46711', // Beacon #9
-      '31392:9354', // Beacon #10
-      // Orange
-      '8696:20374', // Beacon #12
-    ],
-  },
-  {
-    uuid: '25828:43124', // Beacon #5
-    region: 'gray',
-    floor: 7,
-    blocks: [],
-  },
-  {
-    uuid: '33478:1626', // Beacon #6
-    region: 'yellow',
-    floor: 7,
-    blocks: [
-      // Blue
-      '54351:29236', // Beacon #1
-      '38857:8717', // Beacon #13
-    ],
-  },
-  {
-    uuid: '31122:32132', // Beacon #7
-    region: 'gray',
-    floor: 7,
-    blocks: [
-      // pink
-      '34942:3069', // Beacon #4
-      // green
-      '8184:46711', // Beacon #9
-      '31392:9354', // Beacon #10
-    ],
-  },
-  {
-    uuid: '40746:35110', // Beacon #8
-    region: 'gray',
-    floor: 7,
-    blocks: [
-      '8184:46711', // Beacon #9
-      '31392:9354', // Beacon #10
-    ],
-  },
-  {
-    uuid: '8184:46711', // Beacon #9
-    region: 'green',
-    floor: 7,
-    blocks: [],
-  },
-  {
-    uuid: '31392:9354', // Beacon #10
-    region: 'green',
-    floor: 7,
-    blocks: [],
-  },
-  {
-    uuid: '4877:711', // Beacon #11
-    region: 'yellow',
-    floor: 7,
-    blocks: [
-      // Blue
-      '54351:29236', // Beacon #1
-      '38857:8717', // Beacon #13
-    ],
-  },
-  {
-    uuid: '8696:20374', // Beacon #12
-    region: 'orange',
-    floor: 7,
-    blocks: [],
-  },
-  {
-    uuid: '38857:8717', // Beacon #13
-    region: 'blue',
-    floor: 7,
-    blocks: [
-    ],
-  },
-  {
-    uuid: '55025:37051', // Beacon #15
-    region: 'gray',
-    floor: 7,
-    blocks: [],
-  },
-  {
-    uuid: '53981:23804', // Beacon #16
-    region: 'gray',
-    floor: 7,
-    blocks: [],
-  },
-  {
-    uuid: '36732:40211', // Beacon #17
-    region: 'yellow',
-    floor: 7,
-    blocks: [
-      // Blue
-      '54351:29236', // Beacon #1
-      '38857:8717', // Beacon #13
-    ],
-  },
-  {
-    uuid: '6198:51662', // Beacon #18
-    region: 'blue',
-    floor: 7,
-    blocks: [],
-  },
-  {
-    uuid: '34596:40393', // Beacon #14
-    floor: 6,
-    region: 'blue6',
-    blocks: [
-      '53981:23804', // Beacon #16
-      '40746:35110', // Beacon #8
-    ],
-  },
-  {
-    uuid: '62623:22594', // Beacon #2
-    region: 'blue6',
-    floor: 6,
-    blocks: [
-      '53981:23804', // Beacon #16
-      '40746:35110', // Beacon #8
-    ],
-  },
-  {
-    uuid: '60712:26121', // Beacon #19
-    region: 'blue5',
-    floor: 5,
-    blocks: [],
-  },
-  {
-    uuid: '41131:48773', // Beacon #20
-    region: 'blue5',
-    floor: 5,
-    blocks: [],
-  },
-  {
-    uuid: '37048:26826', // Beacon #21
-    region: 'blue4',
-    floor: 4,
-    blocks: [],
-  },
-  {
-    uuid: '21979:16285', // Beacon #22
-    region: 'blue4',
-    floor: 4,
-    blocks: [],
-  },
-  {
-    uuid: '47703:26079', // Beacon #23
-    region: 'blue3',
+    uuid: '17750:62531',
     floor: 3,
+    region: 'T3_3',
     blocks: [],
   },
   {
-    uuid: '48357:24996', // Beacon #24
-    region: 'blue3',
+    uuid: '64331:55483',
     floor: 3,
+    region: 'T3_3',
     blocks: [],
   },
   {
-    uuid: '43326:26582', // Beacon #25
-    region: 'blue2',
-    floor: 2,
+    uuid: '2699:50318',
+    floor: 3,
+    region: 'Table Settings',
     blocks: [],
   },
   {
-    uuid: '8314:60741', // Beacon #26
-    region: 'blue2',
-    floor: 2,
+    uuid: '26186:39016',
+    floor: 3,
+    region: 'Table Settings',
     blocks: [],
   },
   {
-    uuid: '38378:21486', // Beacon #27
-    region: 'blue1',
-    floor: 1,
+    uuid: '25821:11923',
+    floor: 3,
+    region: 'Table Settings',
     blocks: [],
   },
   {
-    uuid: '13116:35432', // Beacon #28
-    region: 'blue1',
-    floor: 1,
+    uuid: '46380:64788',
+    floor: 3,
+    region: 'Table Settings',
     blocks: [],
   },
   {
-    uuid: '14906:11587', // Beacon #29
-    region: 'blue1',
-    floor: 1,
+    uuid: '55254:7275',
+    floor: 3,
+    region: 'Table Settings',
     blocks: [],
   },
   {
-    uuid: '12162:44903', // Beacon #30
-    region: 'blue0',
-    floor: 0,
+    uuid: '37919:4093',
+    floor: 3,
+    region: 'Table Settings',
     blocks: [],
   },
   {
-    uuid: '16195:56093', // Beacon #31
-    region: 'blue0',
-    floor: 0,
+    uuid: '8938:19670',
+    floor: 3,
+    region: 'Gustav Vasa',
+    blocks: [],
+  },
+  {
+    uuid: '50003:7050',
+    floor: 3,
+    region: 'Gustav Vasa',
+    blocks: [],
+  },
+  {
+    uuid: '49406:10306',
+    floor: 3,
+    region: 'Traditions',
+    blocks: [],
+  },
+  {
+    uuid: '8894:22556',
+    floor: 3,
+    region: 'Traditions',
+    blocks: [],
+  },
+  {
+    uuid: '25154:16721',
+    floor: 3,
+    region: 'Traditions',
     blocks: [],
   },
 ];
