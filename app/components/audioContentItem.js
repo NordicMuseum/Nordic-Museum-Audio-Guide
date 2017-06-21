496deaffa057ee381f288b5f762be3e398f8787d
Ruben NIculcea <ruben.niculcea@gmail.com>
2017-06-21 16:12:31 -0400

    Disable and hide transcript button but allow it to take up space

--- app/components/audioContentItem.js
+++ app/components/audioContentItem.js
@@ -17,7 +17,6 @@ export const AUDIO_CONTENT_ITEM_HEIGHT = 45;
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
-    height: AUDIO_CONTENT_ITEM_HEIGHT,
     paddingTop: 8,
     paddingBottom: 16,
     paddingLeft: 15,
@@ -168,14 +167,17 @@ class AudioContentItem extends Component {
                 </View>
               </View>
             </View>
-            {/* <TranscriptButton
+            <TranscriptButton
+              styles={{ opacity: 0 }}
               accessibilityLabel={parseVoiceoverText(I18n.t(audioContent.title))}
-              onPress={() => { this.toggleTranscript(); }}
+              onPress={() => {
+                // this.toggleTranscript();
+              }}
               showTranscript={this.state.transcriptOpened}
-            /> */}
+            />
           </View>
 
-          {/* <Collapsible
+          <Collapsible
             style={this.state.transcriptOpened ? styles.transcript : {}}
             collapsed={!this.state.transcriptOpened}
             duration={collapsibleDuration}
@@ -183,7 +185,7 @@ class AudioContentItem extends Component {
             <View style={styles.transcriptContainer}>
               {breakIntoParagraphTextComponents(I18n.t(audioContent.transcript))}
             </View>
-          </Collapsible> */}
+          </Collapsible>
         </View>
       </TouchableOpacity>
     );
