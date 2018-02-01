33115e1030b1b7cdcd701969799cd816bedbb605
Aron Ambrosiani <aron@ambrosiani.se>
2018-02-01 11:46:05 +0100

    removing duplicate

--- app/data/strings.js
+++ app/data/strings.js
@@ -486,7 +486,6 @@ export const strings = {
     amenitiesScreen_Title: 'Hitta på museet',
     aboutTheAppScreen_Title: 'Om appen',
     aboutScreen_Title: 'Nordiska museet',
-    aboutScreen_Title: 'Nordiska museet',
     aboutScreen_Overview: 'Nordiska museet är Sveriges största kulturhistoriska museum. Ett vardagens palats där berättelser om livet och om människor i Norden, igår, idag och imorgon tar plats. Upptäck kläder och mode, textilier och smycken, möbler och boende, fotografi, leksaker, folkkonst, glas och porslin. Här finns också en utställning om Sveriges enda urfolk, samerna.',
     aboutScreen_AboutHeader: 'Byggnaden',
     aboutScreen_AboutBody: 'Museet grundades redan år 1873 av Artur Hazelius. Den väldiga byggnaden på Djurgården, som är en sevärdhet i sig, är ritad i renässansstil av Isak Gustaf Clason och invigdes 1907.',
