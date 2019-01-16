21aa2b9a3fccd3384005af668f6a76d2e44352f5
Aron Ambrosiani <aron@ambrosiani.se>
2019-01-16 15:32:06 +0100

    updated admission & opening hours

--- app/data/pages/openingHoursAdmission-en.md.js
+++ app/data/pages/openingHoursAdmission-en.md.js
@@ -2,10 +2,12 @@ const text = `# Opening Hours
 
 June–August: daily 9–18
 
-September–May: daily 10–17
+September–May: daily 10–17 (Wednesdays 10–20)
 
 # Admission
 
-Adults 120 SEK, Children/Youth 18 years and under free`;
+Adults: 140 SEK
+Children/Youth 18 years and under: Free
+Tuesday 13.00–17.00 (Sep–May): Free`;
 
 export default text;
