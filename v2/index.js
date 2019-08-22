0cff8705c0835e829ebd5b0fe70dcdba483c4d19
Ruben Niculcea <ruben.niculcea@gmail.com>
2019-08-22 16:49:13 -0400

    Basic compiling working

---
+++ v2/index.js
@@ -0,0 +1,14 @@
+import {Navigation} from 'react-native-navigation';
+import App from './App';
+
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'navigation.playground.WelcomeScreen',
+      },
+    },
+  });
+});
