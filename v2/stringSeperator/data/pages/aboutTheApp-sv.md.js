8cad201e9e6ee73b61e7f47cd37942ace8811364
Sam Ticknor <ticknors@carnegiemuseums.org>
2019-08-27 14:09:27 -0400

    Created string separator node script, seperates AudioContent from TourStops from UIStrings

---
+++ v2/stringSeperator/data/pages/aboutTheApp-sv.md.js
@@ -0,0 +1,58 @@
+const text = `# Ljudinnehåll
+
+Ljudinnehållet i appen har spelats in under åtminstone tjugo år. Senare tillägg har spelats in av Eskimo Avenue och H Olsson Ord & Vision med Robert Ziherl som projektledare på Nordiska museet. Höjdpunkterna är producerade av H Olsson Ord & Vision i samarbete med Lena Landerberg och Robert Ziherl som projektledare på Nordiska museet.
+
+Musiken i appen kommer från den musikhistoriska serien *Musica Sveciae* som administreras av Kungl. Musikaliska Akademien.
+
+# Appen
+
+Nordiska museets audioguide har utvecklats tillsammans med the Innovation Studio vid Carnegie Institute i Pittsburgh, USA. Appen är en vidareutvecklad version av The Warhol Museums app Warhol Out Loud. Båda apparna är släppta som öppen källkod.
+
+# Projektledare, Nordiska museet
+
+Aron Ambrosiani
+
+# Projektledare, Carnegie Institute
+
+Ruben Niculcea
+
+# Utveckling och design
+
+Ruben Niculcea, Carnegie Institute
+Sam Ticknor, Carnegie Institute
+Aron Ambrosiani, Nordiska museet
+Robert Ziherl, Nordiska museet
+
+# Styrgrupp, Nordiska museet
+
+Vanessa Gandy
+Loredana Jelmini
+Sven Rentzhog
+
+# Appikon
+
+Ann-Sofi Marminge Design
+
+# Översättningar
+
+BTI Studios
+
+# Fotografier
+
+Välkomstbild: NMA.03906 Foto: Karl-Erik Granath
+Höjdpunkter: NMA.0030770 Foto: Karl Heinz Hernried
+Barnens audioguide: Illustration av Ann-Sofi Marminge
+Traditioner: NMA.0029961 Foto: Karl Heinz Hernried
+Dukade bord: NMA.0036167 Foto: Karl-Erik Granath
+Textilgalleriet: NMA.0027979 Foto: Mats Landin
+Sápmi: NMA.0056303 Foto: Rolf Kjellström
+Folkkonst: NM.0050874 Foto: Karolina Kristensson
+Hem och bostad: NMA.0033276 Foto: Okänd © Nordiska museets arkiv
+
+Bild på Nordiska museet: fotograf John Swan/Canopy Aero
+
+# Integritetspolicy
+
+https://www.nordiskamuseet.se/om-museet/integritetspolicy`;
+
+export default text;
