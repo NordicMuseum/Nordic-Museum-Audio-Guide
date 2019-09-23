18bf3a3f6f50223cba301cbbd505175796d9822c
Aron Ambrosiani <aron@ambrosiani.se>
2019-09-23 19:49:35 +0200

    additional appcenter script steps

--- v2/appcenter-post-clone.sh
+++ v2/appcenter-post-clone.sh
@@ -4,4 +4,6 @@ rm -rf images
 git clone https://$USER:$TOKEN@github.com/NordicMuseum/Nordic-Museum-Audio-Guide-Assets.git
 mv Nordic-Museum-Audio-Guide-Assets/audio/ audio
 mv Nordic-Museum-Audio-Guide-Assets/images/ images
+mv Nordic-Museum-Audio-Guide-Assets/secrets/appcenter-config.json ../../android/app/src/main/assets/appcenter-config.json
+mv Nordic-Museum-Audio-Guide-Assets/secrets/AppCenter-Config.plist ../../ios/AppCenter-Config.plist
 rm -rf Nordic-Museum-Audio-Guide-Assets
