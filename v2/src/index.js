6326a50fd043560f52cdbe70e905eca05f08997a
Sam Ticknor <srticknor@gmail.com>
2019-10-04 15:08:22 -0400

    Imported platform

--- v2/src/index.js
+++ v2/src/index.js
@@ -1,4 +1,5 @@
 import { Navigation } from 'react-native-navigation';
+import { Platform } from 'react-native';
 
 import AsyncStorage from '@react-native-community/async-storage';
 import DeviceInfo from 'react-native-device-info';
