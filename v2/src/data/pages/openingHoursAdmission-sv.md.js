ca6e21ab20ec8c3c917bb5691a699b22e8cc6d20
Aron Ambrosiani <aron@ambrosiani.se>
2019-12-03 10:47:47 +0100

    include link to current info

--- v2/src/data/pages/openingHoursAdmission-sv.md.js
+++ v2/src/data/pages/openingHoursAdmission-sv.md.js
@@ -9,6 +9,8 @@ September–maj: dagligen kl 10–17 (onsdag kl. 10–20)
 Vuxen och student: 140 kr (fri entré tisdag 13–17 september–maj)
 Pensionär: 120 kr (fri entré tisdag kl. 13–17 september–maj)
 Barn och ungdomar till och med 18 år: fri entré
-Lekstugan: Barn 40 kr (ingår för vuxna i entréavgift till museet)`;
+Lekstugan: Barn 40 kr (ingår för vuxna i entréavgift till museet)
+
+[Aktuell information](https://www.nordiskamuseet.se/besok-oss/planera-besok/valkommen-till-nordiska-museet)`;
 
 export default text;
