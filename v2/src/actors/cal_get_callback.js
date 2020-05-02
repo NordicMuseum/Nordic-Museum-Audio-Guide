2f5ffd8bc334c0853dd2651b0cc236d0bfb98c3c
Christoffer Nilsson <christoffernilsson@Christoffers-MacBook-Air.local>
2020-05-02 16:07:23 +0200

    following the guides, this is what we get, see photo for context (email)

---
+++ v2/src/actors/cal_get_callback.js
@@ -0,0 +1,84 @@
+var XMLHttpRequest = new XMLHttpsRequest();
+
+function processResult(lines, dayOnly) {
+  var today = new Date();
+  var date_now_f = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
+  HAR_HITTAT_VEVENT = false;
+  found_min_one = false;
+  var events = []; /*initalize JS object */
+  var events_i = 0; /*COUNTER */
+
+  for (i = 0; i < lines.length; i++) {
+    if (lines[i].includes('BEGIN:VEVENT')) {
+      HAR_HITTAT_VEVENT = true;
+      events[events_i] = { date: "", time: "", title: "", desc: "", URL: "" };
+    }
+    if (lines[i].includes('DTSTART') && HAR_HITTAT_VEVENT) {
+      var date_raw = lines[i].split(":");
+      var date_tot = date_raw[1].replace('\r', '').split("T")
+      var date_f = date_tot[0].slice(0, 4) + '-' + date_tot[0].slice(4, 6) + '-' + date_tot[0].slice(6, 8);
+      events[events_i]["date"] = date_f;
+      if (lines[i].length == 46) {
+        var time_f = date_tot[1].slice(0, 2) + ':' + date_tot[1].slice(2, 4);
+        events[events_i]["time"] = time_f;
+      }
+    }
+    else if (lines[i].includes('SUMMARY') && HAR_HITTAT_VEVENT) {
+      var title = lines[i].split(":");
+      events[events_i]["title"] = title[1].replace('\r', '');
+    }
+    else if (lines[i].includes('DESCRIPTION') && HAR_HITTAT_VEVENT) {
+      var desc = lines[i].split("DESCRIPTION:");
+      events[events_i]["desc"] = desc[1];
+    }
+    else if (lines[i].includes('URL;TYPE=URI') && HAR_HITTAT_VEVENT) {
+      var URL = lines[i].split("URL;TYPE=URI:");
+      events[events_i]["URL"] = URL[1].replace('\r', '');
+    }
+    else if (lines[i].includes('END:VEVENT') && HAR_HITTAT_VEVENT) {
+      //console.log('_'+events[events_i]["date"]+'_', "IS NOT EQUAL? ", '_'+date_now_f+'_')
+      if (dayOnly && (events[events_i]["date"] != date_now_f)) {
+        HAR_HITTAT_VEVENT = false;
+      }
+      else {
+        events_i++;
+        HAR_HITTAT_VEVENT = false;
+        if (found_min_one == false) found_min_one = true;
+      }
+    }
+
+  }
+  if (found_min_one) {
+    var final = events;
+  }
+  else { var final = "NO_EVENTS" };
+  return final;
+};
+
+module.exports = function () {
+  this.getCalStr = function (debugMode, dayOnly, callback) {
+    var request = new XMLHttpRequest();
+    url = debugMode ? "https://www.calendarlabs.com/ical-calendar/ics/71/Sweden_Holidays.ics" : "https://www.nordiskamuseet.se/calendar/ical/ical/calendar-nordiska-museet.ics";
+    request.open(
+      "GET",
+      url,
+      true
+    )
+    request.send(null);
+    request.onreadystatechange = function () {
+      //checks if response is ready.
+      if (request.readyState === 4 && request.status === 200) {
+        var type = request.getResponseHeader("Content-Type");
+        if (type.indexOf("text") !== 1) {
+          var lines = request.responseText.split("\n");
+          // this is where we process the XMLHttpRequest
+          final = processResult(lines, dayOnly)
+          if (callback) return callback(final);
+          // END OF if (type.indexOf("text") !== 1)
+        }
+        // END OF if (request.readyState === 4 && request.status === 200)
+      }
+      // END OF: request.onreadystatechange
+    }
+  };
+}
--- v2/src/actors/downloadEvents.js
+++ v2/src/actors/downloadEvents.js
@@ -1,4 +1,8 @@
 import { updateEvents } from "../actions/device";
+require("./cal_get_promise.js")();
+//const https = require("https");
+//const fs = require("fs");
+
 
 class DownloadEventsActor {
   constructor(store) {
@@ -6,17 +10,25 @@ class DownloadEventsActor {
     this._dispatch = store.dispatch;
 
     // const tenMins = 1000 * 60 * 10;
-    const tenMins = 1000;
+    const tenMins = 1500;
+    var count = 0;
+    const debugMode = true;
+    var testModule = new XMLHttpsRequest();
+    urlS = debugMode ? "https://www.calendarlabs.com/ical-calendar/ics/71/Sweden_Holidays.ics" : "https://www.nordiskamuseet.se/calendar/ical/ical/calendar-nordiska-museet.ics";
+    //const file = fs.createWriteStream("cal_raw.txt");
 
-    setInterval(() => {
+    setInterval(async () => {
       const currentTime = new Date();
-      const currentTimeStr = currentTime.toISOString();
+      const currentTimeStr = await currentTime.toISOString();
+      ++count;
+      resultstr = await getCalStr(false, true);
 
       this._dispatch(
         updateEvents({
           //Download and store the current dates events
-          //as an array of strings.
-          [currentTimeStr]: ["football", "playdate"]
+          //as an array of strings
+
+          ["test"]: ["football"] [resultstr]
         })
       );
     }, tenMins);
